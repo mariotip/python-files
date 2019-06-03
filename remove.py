c08fbb460cdad67ff3edf844cf4b44970ada8e20
Mario tip <ing_mariomcs@outlook.com>
2019-06-02 21:49:39 -0500

    ultimate change
    rename list first name quit if is same name

--- remove.py
+++ remove.py
@@ -1,8 +1,16 @@
-import os
 os.chdir('C:/Users/Ing. Mario/Documents/python/animales/png')
+
 for f in os.listdir():
+
+    ext = os.path.splitext(f)[-1]
+    # print (os.path.splitext(f)[-1])
+
     files = os.path.splitext(f)[0]
     name_large = files.split(' ')
     if len(name_large) > 1 :
         print(len(name_large))
-        os.remove(files+'.png')
+        final = files.split(' ')[0]
+        if os.path.exists(final + ext):
+            os.remove(files + ext)
+        else:
+            os.rename(f,final + ext)
