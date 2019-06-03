9dae0d9e84c9e1026611d093ceb56dcf09e2712c
Mario tip <ing_mariomcs@outlook.com>
2019-06-02 21:54:47 -0500

    Update remove.py

--- remove.py
+++ remove.py
@@ -1,14 +1,15 @@
-os.chdir('C:/Users/Ing. Mario/Documents/python/animales/png')
+
+import os
+
+os.chdir('C:/Users/Ing. Mario/Documents/python/animales')
 
 for f in os.listdir():
 
     ext = os.path.splitext(f)[-1]
-    # print (os.path.splitext(f)[-1])
-
     files = os.path.splitext(f)[0]
+
     name_large = files.split(' ')
     if len(name_large) > 1 :
-        print(len(name_large))
         final = files.split(' ')[0]
         if os.path.exists(final + ext):
             os.remove(files + ext)
