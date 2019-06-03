9765cd54e652309ded7c7b62e83936c8cf902c57
Mario tip <ing_mariomcs@outlook.com>
2019-06-02 21:16:22 -0500

    Update remove.py

--- remove.py
+++ remove.py
@@ -1,21 +1,8 @@
 import os
-# import request
 os.chdir('C:/Users/Ing. Mario/Documents/python/animales/png')
 for f in os.listdir():
-    # name_files = os.path.(f)
-    # dts = str(name_files) + '.jpg'
-    # os.path.splitext("sample.txt")[0]
-    # print(str(f).split('.')[0])
     files = os.path.splitext(f)[0]
     name_large = files.split(' ')
     if len(name_large) > 1 :
         print(len(name_large))
         os.remove(files+'.png')
-
-    # else
-    #     os.rename(f,final+'.jpg')
-
-    # final = files.split(' ')[0]
-    # if os.path.exists(final+'.jpg'):
-    # else:
-    #     print('el archivo no existe')
